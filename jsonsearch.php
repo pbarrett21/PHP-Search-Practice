5a1fb090b6121875e2d7bade7f111da5cda30ae5
Paul Barrett <fastturtle4@gmail.com>
2018-11-13 21:25:21 -0500

    iterate through json

--- jsonsearch.php
+++ jsonsearch.php
@@ -1,5 +1,41 @@
-# Paul Barrett
-
 <?php
+// Paul Barett
+$NOBELPHYSICS = "./Nobel_physics.json";
 echo "Hello world!\n";
+
+$line = readline("search for: ");
+echo "$line";
+
+function readGivenJson($filename){
+	if(file_exists($filename)){
+		$jsoncontents = file_get_contents($filename);
+		$decodedjson = json_decode($jsoncontents);
+		
+		$subjson = '';
+		foreach ($decodedjson->prizes as $key => $value) {
+			foreach($value as $subkey => $subvalue){
+				echo "$subkey\n";
+			}
+		}
+
+		/*foreach ($decodedjson[$subjson] as $subkey => $subvalue) {
+			echo "$subkey = $subvalue\n";
+		}*/
+
+	} else {
+		echo "ERROR READING FILE\n";
+	}
+}
+
+readGivenJson($NOBELPHYSICS);
+
+
+
+
+
+
+
+
+
+
 ?>
