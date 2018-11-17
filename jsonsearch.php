f9a3a7d23321522f0a394737a8ab96c00ca22827
Paul Barrett <fastturtle4@gmail.com>
2018-11-17 13:52:51 -0500

    added html output
    http://cs.uky.edu/~pba243/P4/jsonsearch.php

--- jsonsearch.php
+++ jsonsearch.php
@@ -3,78 +3,42 @@
 $NOBELPHYSICS = "./Nobel_physics.json";
 $OSCAR = "./2018_Oscar.json";
 
-$DATASOURCES = file_get_contents("./DataSources.json");
-$dogs = "Nobel - Physics";
-//var_dump($cats->$dogs);
-
-
-//echo "Hello world!\n";
-
-//$line = readline("search for: ");
-//echo "$line";
-
-/*
-function doSearch($file, $category, $subcat, $searchterm){
-	
-	$decData = json_decode($file);
-	$categs = $decData->categories;
-	$srchterms = $decData->searchterms;
-
-	$whichcat = $categs->$category;		//json name of category (ex: NobelPhysics.json)
-	$whichterm = $srchterms->$subcat;	//category of that json to seach inside
-
-	if(file_exists($whichcat)){
-		$jsearch = file_get_contents($whichcat);
-		$searchjson = json_decode($jsearch);
-
-		//foreach(
-	}else{
-		echo "File doesn't even exist, yo";
-	}
-}
-
-doSearch("$DATASOURCES", "Nobel - Physics", "F0", "yeet");
- */
+$DATASOURCES = "./DataSources.json";
 
 $readyet = "";
-function readGivenJson($filename, $changer, $newval, $category, $searchterm){
+function doSearch($filename, $changer, $newval, $searchcategory, $searchterm){
 
-	//$readyet = "";
 	if(file_exists($filename)){
 
-		if($GLOBALS['readyet'] != $filename){	//only read and decode any file once. 
+		if($GLOBALS['readyet'] != $filename){	//only read and decode any file once instead of once per recursion 
 			$jsoncontents = file_get_contents($filename);
 			$decodedjson = json_decode($jsoncontents, true);
 			$GLOBALS['readyet'] = $filename;
-			echo "Category: "; echo $category;
+			echo "File: "; echo $filename;
+			echo "; Category: "; echo $searchcategory;
 			echo "; Search Term: "; echo $searchterm;
 			echo "\n";
 		}
 
-		if($changer != false){
+		if($changer != false){	//change value of $decodedjson in foreach to go into subjsons
 			$decodedjson = $newval;
 		}
 
-		foreach($decodedjson as $key=>$val){
+		foreach($decodedjson as $key=>$val){	//search through json and output search results
+			
 			if(gettype($val)=="array"){	
-				readGivenJson($filename, true, $val, $category, $searchterm);		
+				doSearch($filename, true, $val, $searchcategory, $searchterm);		
 			} else {
 				if($val == $searchterm){
+			
 					foreach($decodedjson as $k=>$v){
-						echo $v; echo "\n";
+						echo $k; echo ": "; echo $v; echo "\n";
 						//if its an array do the thing to print it
 						if(gettype($val)=="array"){
-							readGivenJson($filename, true, $val, $category, $searchterm);	
+							doSearch($filename, true, $val, $searchcategory, $searchterm);	
 						}
 					}
 				}
-				/*
-				echo "Key = ";
-			 	echo $key;
-				echo " Val = ";	
-				echo $val;
-				echo "\n";
-				 */
 			}
 		}
 		
@@ -82,6 +46,73 @@ function readGivenJson($filename, $changer, $newval, $category, $searchterm){
 		echo "ERROR READING FILE\n";
 	}
 }
-readGivenJson($OSCAR, false, [], "firstname", "Daniel");
+#doSearch($DATASOURCES, false, [], "firstname", "Peter");
+
+function getJsonCategories($filename) {
+	if(file_exists($filename)){
+		$filecontents = file_get_contents($filename);
+		$decodejson = json_decode($filecontents, true);
+		
+		$catcount = 0;
+		$categoryarray = array();
+		foreach($decodejson["categories"] as $key=>$val){
+			$categoryarray[$catcount] = $key;
+			$catcount++;
+		}
+		return $categoryarray;
+	} else {
+		echo "File doesn't exist";
+	}
+}
+
+function getJsonSearchTerms($filename){
+	if(file_exists($filename)){
+		$filecontents = file_get_contents($filename);
+		$decodejson = json_decode($filecontents, true);
+
+		$searchcount = 0;
+		$searchtermarray = array();
+		foreach($decodejson["searchterms"] as $skey=>$sval){
+			$searchtermarray[$searchcount] = $sval;
+			$searchcount++;
+		}
+		return $searchtermarray;
+	} else {
+		echo "Make sure the file is correct";
+	}
+}
+$catray = getJsonCategories($DATASOURCES);
+$searchray = getJsonSearchTerms($DATASOURCES);
+
+function echoArray($array){
+	for($iter = 0; $iter < count($array); $iter++){
+		echo '<option value = "';
+		echo $array[$iter];
+		echo '">';
+		echo $array[$iter];
+		echo "</option>\n";
+	}
+}
+
+#echoArray($catray);
+
+?>
+<html>
+	<body>
+		<FORM action="jsonsearch.php" method="get">
+			<select id = "Category" class = "dropdown">
+				<?php echoArray($catray); ?>
+			</select>
+			<br>
+			<select id = "Searchterm" class = "dropdown">
+				<?php echoArray($searchray); ?>
+			</select>
+			<br>
+			<input type = "text" name = "searchquery" class = "searchbox">
+			<INPUT type="submit" value="   Submit   ">
+		</FORM>
+	</body>
+</html>
+<?php
 
 ?>
